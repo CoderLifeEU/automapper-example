69ec75be7aa3be3b99d8efaf5d912835f6a46f1b
CoderLife <coderlifeeu@gmail.com>
2014-05-31 16:36:07 +0300

    CoderLifeEU View Models https://github.com/CoderLifeEU/automapper-example

---
+++ ViewModels/ViewModels/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ViewModels.ViewModel;
+using AutoMapper;
+using ViewModels.Models;
+
+namespace ViewModels.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            CreateUserViewModel model = new CreateUserViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Register(CreateUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+               User user = AutoMapper.Mapper.Map<CreateUserViewModel, User>(model);
+                return RedirectToAction("Success");
+                // Create User code           
+            }
+            return View();
+        }
+
+        public ActionResult Success()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ ViewModels/ViewModels/ViewModel/Mappings/ViewModelToDomainMappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ViewModels.Models;
+
+namespace ViewModels.ViewModel.Mappings
+{
+    public class ViewModelToDomainMappingProfile : Profile
+    {
+        public override string ProfileName
+        {
+            get { return "ViewModelToDomainMappings"; }
+        }
+
+        protected override void Configure()
+        {
+            Mapper.CreateMap<CreateUserViewModel, User>()
+                .ForMember(cv => cv.Name, m => m.MapFrom(foo => foo.Name))
+                .ForMember(cv => cv.Password, m => m.MapFrom(foo => foo.Password));
+        }
+    }
+}
